9511765d1a519a6906881771def9f2d7f8622efa
pauuuutomas <paulatomas1901@gmail.com>
2026-09-18 16:10:53 -0300

    agregar schemas a rutas de productos

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,47 +1,107 @@
+import {
+  ProductCreateBody,
+  ProductUpdateBody,
+  ProductFiltersQuery,
+  ProductSchema,
+  IdParam,
+} from "../schemas/index.js";
+
 export default async function productRoutes(app) {
-  // Listar productos
-  // Más adelante tendrá paginación y filtros.
-  app.get("/productos", async (request) => {
-    return {
-      message: "Listar productos",
-      query: request.query,
-    };
-  });
+  // Listar y filtrar productos
+  app.get(
+    "/productos",
+    {
+      schema: {
+        querystring: ProductFiltersQuery,
+        response: {
+          200: {
+            type: "array",
+            items: ProductSchema,
+          },
+        },
+      },
+    },
+    async (request) => {
+      return {
+        message: "Listar productos",
+        query: request.query,
+      };
+    },
+  );
 
   // Obtener producto por ID
-  app.get("/productos/:id", async (request) => {
-    const { id } = request.params;
+  app.get(
+    "/productos/:id",
+    {
+      schema: {
+        params: IdParam,
+        response: {
+          200: ProductSchema,
+        },
+      },
+    },
+    async (request) => {
+      const { id } = request.params;
 
-    return {
-      message: "Obtener producto",
-      id: Number(id),
-    };
-  });
+      return {
+        id: Number(id),
+      };
+    },
+  );
 
   // Crear producto
-  app.post("/productos", async () => {
-    return {
-      message: "Crear producto",
-    };
-  });
+  app.post(
+    "/productos",
+    {
+      schema: {
+        body: ProductCreateBody,
+        response: {
+          201: ProductSchema,
+        },
+      },
+    },
+    async () => {
+      return {
+        id: 1,
+      };
+    },
+  );
 
   // Modificar producto
-  app.patch("/productos/:id", async (request) => {
-    const { id } = request.params;
+  app.patch(
+    "/productos/:id",
+    {
+      schema: {
+        params: IdParam,
+        body: ProductUpdateBody,
+        response: {
+          200: ProductSchema,
+        },
+      },
+    },
+    async (request) => {
+      const { id } = request.params;
 
-    return {
-      message: "Modificar producto",
-      id: Number(id),
-    };
-  });
+      return {
+        id: Number(id),
+      };
+    },
+  );
 
-  // Desactivar/eliminar producto
-  app.delete("/productos/:id", async (request) => {
-    const { id } = request.params;
+  // Desactivar producto
+  app.delete(
+    "/productos/:id",
+    {
+      schema: {
+        params: IdParam,
+      },
+    },
+    async (request, reply) => {
+      const { id } = request.params;
 
-    return {
-      message: "Desactivar producto",
-      id: Number(id),
-    };
-  });
+      console.log(`Desactivando producto ${id}`);
+
+      return reply.code(204).send();
+    },
+  );
 }
